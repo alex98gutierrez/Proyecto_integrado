051c2b486e825e254857692d791e6425fccaa619
alex98gutierrez <57136870+alex98gutierrez@users.noreply.github.com>
2022-05-13 08:32:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+  echo "<link rel="stylesheet" href="styles.css">";
+echo "</head>";
+echo "<body>";
+
+echo "<h1>This is a heading</h1>";
+echo "<p>This is a paragraph.</p>";
+
+echo "</body>";
+echo "</html>";
 echo "Hello World!";
+
+?>
