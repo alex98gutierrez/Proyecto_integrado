087798bccfc266f552f73e38fc7789fa6606baa6
alex98gutierrez <alex.gutierrez.pineda@gmail.com>
2022-06-04 12:59:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,42 +6,6 @@ if ($connect->connect_errno) {
 }
 
 
-echo "<!DOCTYPE html>";
-echo "<html lang='es'>";
-	echo "<head>";
-		echo "<meta charset='UTF-8'>";
-	  	#echo "<link rel="stylesheet" href="styles.css">";
-
-	  	echo "<title>El planeta de los videos</title>";
-
-	echo "</head>";
-
-	echo "<body>";
-
-	echo "<h1>This is a heading</h1>";
-
-		echo "<p>This is a paragraph.</p>";
-
-	echo "<header>";
-
-        echo "<h2 class='logo'>El planeta de los videos";
-
-	echo "</header>";
-
-        echo "<nav>";
-            echo "<a href=''>Inicio</a>";
-            echo "<a href=''>Películas</a>";
-            echo "<a href=''>Series</a>";
-            echo "<a href=''>Últimos estrenos</a>";
-            echo "";
-        echo "</nav>";
-
-	echo "<p>CAMBIOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOS</p>";
-        
     echo "<p>Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>";
-
-	echo "</body>";
-echo "</html>";
-
 
 ?>
